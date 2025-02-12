77f33eb0dddbd1dbf06b243782519ed3adc00e64
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-02-12 21:39:05 +0530

    application

--- server/techlearn/techlearn/wsgi.py
+++ server/techlearn/techlearn/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techlearn.settings')
 
-app = get_wsgi_application()  # Vercel expects "app" as the entry point
+application = get_wsgi_application()
